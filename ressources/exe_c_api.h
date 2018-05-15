1797186ebc5af51cf810d3b21eee9b4be1ad3a7f
Thomas WACHE <thomas@wache.fr>
2018-05-15 15:25:37 +0200

    Added C header file to use call rust functions in C

---
+++ ressources/exe_c_api.h
@@ -0,0 +1,49 @@
+#ifndef __EXE_C_API__
+#define __EXE_C_API__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+
+typedef void* rs_handle_t;
+typedef struct {
+    size_t (*get_number_of_sections)(rs_handle_t *h);
+    rs_handle_t (*get_section_at)(rs_handle_t *h, size_t idx);
+    const char* (*get_section_name_at)(rs_handle_t *h, size_t idx);
+    uint32_t (*get_flags)(rs_handle_t *h);
+    size_t (*get_size)(rs_handle_t *h);
+    size_t (*get_offset)(rs_handle_t *h);
+    void (*free_exe)(rs_handle_t *h);
+} rs_ops_t;
+
+#define GENERATE_BINDINGS(prefix) \
+    extern rs_handle_t prefix##_parse(const uint8_t *data, size_t len); \
+    extern size_t prefix##_get_number_of_sections(rs_handle_t *h); \
+    extern rs_handle_t prefix##_get_section_at(rs_handle_t *h, size_t idx); \
+    extern const char* prefix##_get_section_name_at(rs_handle_t *h, size_t idx); \
+    extern uint32_t prefix##_get_flags(rs_handle_t *h); \
+    extern size_t prefix##_get_size(rs_handle_t *h); \
+    extern size_t prefix##_get_offset(rs_handle_t *h); \
+    extern void prefix##_free_exe(rs_handle_t *h); \
+    \
+    const rs_ops_t prefix##_ops = { \
+        .get_number_of_sections = prefix##_get_number_of_sections, \
+        .get_section_at = prefix##_get_section_at, \
+        .get_section_name_at = prefix##_get_section_name_at, \
+        .get_flags = prefix##_get_flags, \
+        .get_size = prefix##_get_size, \
+        .get_offset = prefix##_get_offset, \
+        .free_exe = prefix##_free_exe \
+    }; \
+    typedef struct { \
+        rs_handle_t handle; \
+        const rs_ops_t *ops; \
+    } prefix##_handle_t; \
+    \
+    static inline bool prefix##_parse_helper(prefix##_handle_t *ptr, const uint8_t *data, size_t len) { \
+        ptr->handle = prefix##_parse(data, len); \
+        ptr->ops = & prefix##_ops; \
+        return ptr->handle == NULL ? false : true; \
+    }
+
+#endif // __EXE_C_API__
